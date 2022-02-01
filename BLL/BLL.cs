45a901121780807654eb688f86aac0e261312769
Anderson <97201511+AndersonGomezM@users.noreply.github.com>
2022-02-01 19:50:14 -0400

    BLL vacío creado

---
+++ BLL/BLL.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anderson_Gomez_Ap1_p1.BLL
+{
+    public class BLL
+    {
+        
+    }
+}
