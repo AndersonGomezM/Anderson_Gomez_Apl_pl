06a452603538dd2ecea360c0171ce01c7273e7d2
Anderson <97201511+AndersonGomezM@users.noreply.github.com>
2022-02-01 19:13:59 -0400

    Proyecto creado y se instalaron las herramientas de EntityFrameworkCore

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anderson_Gomez_Ap1_p1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
