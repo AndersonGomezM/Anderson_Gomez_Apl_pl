51a32bff4fe26ae9a654b20343e417af5f11409e
Anderson <97201511+AndersonGomezM@users.noreply.github.com>
2022-02-01 19:35:05 -0400

    Contexto creado con el metodo OnConfiguring creado para la base de datos

---
+++ DAL/Contexto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Anderson_Gomez_Ap1_p1.DAL
+{
+    public class Contexto:DbContext
+    {
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite(@"Data Source=Data\BaseDeDatos.db");
+    }
+    }
+}
