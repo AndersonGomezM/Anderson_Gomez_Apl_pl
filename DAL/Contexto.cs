80bbd07f5b7b4fc70a93e7665ffbcd8e56955077
Anderson <97201511+AndersonGomezM@users.noreply.github.com>
2022-02-07 18:55:49 -0400

    Crear contexto para la base de datos con la entidades

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,15 +1,17 @@
 using System;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
+using Anderson_Gomez_Ap1_p1.Entidades;
 
 namespace Anderson_Gomez_Ap1_p1.DAL
 {
-    public class Contexto:DbContext
+    public class Contexto : DbContext
     {
+        public DbSet<Productos>? Entidades { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlite(@"Data Source=DATA\BaseDeDatos.db");
-    }
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\ProductosBaseDeDatos.db");
+        }
     }
 }
